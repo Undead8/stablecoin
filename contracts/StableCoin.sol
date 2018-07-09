9f34dae080138d40d67074e5c8517f3aa0c9fb0e
Undead8 <cousineau915@hotmail.com>
2018-07-08 20:47:28 -0400

    Initial commit

---
+++ contracts/StableCoin.sol
@@ -0,0 +1,98 @@
+pragma solidity ^0.4.24;
+
+import './SuperOwnable.sol';
+import './Pausable.sol';
+
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external; }
+
+contract StableCoin is SuperOwnable, Pausable {
+    string public name;
+    string public symbol;
+    uint8 public decimals = 2;
+    uint256 public totalSupply;
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
+
+    constructor(string tokenName, string tokenSymbol) public {
+        name = tokenName;
+        symbol = tokenSymbol;
+    }
+
+    function _transfer(address _from, address _to, uint _value) internal {
+        require(_to != 0x0, "Cannot transfer to 0x0 address.");
+        require(balanceOf[_from] >= _value, "Insufficient balance.");
+        require(balanceOf[_to] + _value > balanceOf[_to], "Overflow.");
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+    function transfer(address _to, uint256 _value) public whenNotPaused returns (bool success) {
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool success) {
+        require(_value <= allowance[_from][msg.sender], "Insufficient allowance.");
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public whenNotPaused returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public whenNotPaused returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+
+    function burn(uint256 _value) public whenNotPaused returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance.");
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+
+    function mintToken(address target, uint256 mintedAmount) public onlyOwner whenNotPaused {
+        balanceOf[target] += mintedAmount;
+        totalSupply += mintedAmount;
+        emit Transfer(0, this, mintedAmount);
+        emit Transfer(this, target, mintedAmount);
+    }
+
+    function destroy() public payable onlySuperOwner {
+        selfdestruct(superOwner);
+    }
+
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    /* function burnFrom(address _from, uint256 _value) public returns (bool success) {
+    require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+    require(_value <= allowance[_from][msg.sender]);    // Check allowance
+    balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+    allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+    totalSupply -= _value;                              // Update totalSupply
+    emit Burn(_from, _value);
+    return true;
+    }
+    */
+}
