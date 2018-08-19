5d12f74935176211a82accbcb9fd6910a22cf735
Youri Cousineau <cousineau915@hotmail.com>
2018-08-19 10:38:59 -0400

    Add minimum cooldown and remove maximum

--- contracts/Cooldownable.sol
+++ contracts/Cooldownable.sol
@@ -10,7 +10,16 @@ contract Cooldownable is Ownable {
         uint256 value;
     }
 
+    uint256 public minimumCooldown;
+
     mapping (address => TokenInCooldown) internal cooldownOf;
+
+    event newMinimumCooldown(uint256 minutes);
+
+    function setMinimumCooldown(uint256 _minutes) public onlyOwner {
+        minimumCooldown = _minutes;
+        emit newMinimumCooldown(_minutes);
+    }
 
     function valueInCooldown(address _target) public view returns (uint256 value) {
         uint256 expiration = cooldownOf[_target].cooldownExpiration;
@@ -38,7 +47,7 @@ contract Cooldownable is Ownable {
     }
 
     function setCooldown(address _target, uint256 _value, uint256 _minutes) internal {
-        require(_minutes <= 7200, "Max cooldown is 7200 minutes");
+        require(msg.sender == owner || _minutes >= minimumCooldown, "Cooldown is lower than minimum cooldown.");
         cooldownOf[_target] = TokenInCooldown(now + _minutes * 1 minutes, _value);
     }
 }
