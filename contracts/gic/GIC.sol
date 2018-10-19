0188735eccabf36f8c72c18c70c121855fc3b537
Youri Cousineau <cousineau915@hotmail.com>
2018-10-18 22:29:17 -0400

    wip

--- contracts/gic/GIC.sol
+++ contracts/gic/GIC.sol
@@ -1,7 +1,18 @@
 pragma solidity ^0.4.24;
 
+import "../stablecoin/Pausable.sol";
+import "../stablecoin/Authorizable.sol";
 
-contract GIC {
-  constructor() {
-  }
+
+contract GIC is Pausable, Authorizable {
+    address public currencyContractAddress;
+
+    mapping (uint256 => uint256) public currentRates;
+
+// Constructor should get the token name and token symbol from currencyContract
+    constructor(address _currencyContract) {
+        currencyContract = _currencyContract;
+
+    }
+
 }
